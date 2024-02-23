a0b2133d0863d84f65992fba314f7a03e07cb81b
Jay_ <ekdmd9092@naver.com>
2024-02-23 14:42:41 +0900

    Initial commit

---
+++ src/test/kotlin/com/example/udo/UdoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.udo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UdoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
